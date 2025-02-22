5e642350b15a61ed9e378f5dba52a5f9a6af86b3
Sergio <sergiolopezelio@hotmail.com>
2025-02-22 13:27:02 +0100

    Mejora detalle_mascota.html y protectoras.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        customPurple: '#CBABD4',
-        customWhite: '#FFFFFF',
-        customBlue: '#4F42E1',
-        customBlack: '#303030',
-      },
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-};
-
-
+	content: ['./index.html', './src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				customPurple: '#CBABD4',
+				customWhite: '#FFFFFF',
+				customBlue: '#4F42E1',
+				customBlack: '#303030',
+			},
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif'],
+				roboto: ['Roboto', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+}
