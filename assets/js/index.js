db4b264f68e5f8926a5b3b88cbed8c1b6d9b3cd7
Adur Marques <adur.marques@gmail.com>
2025-02-22 22:06:29 +0100

    Se añaden idProtectora, se unifica los estilos de los botones para que tengan el mismo borde, se mueve el js a un archivo común, se arreglan colores y tamaños de páginas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-const URL_BACKEND = 'https://adoptamegrupo8.alphapro.es';
+const URL_BACKEND = 'https://adoptamegrupo8.alphapro.es'
+
+const menuBtn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+menuBtn.addEventListener('click', () => {
+	menu.classList.toggle('hidden')
+})
