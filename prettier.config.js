fdc4cfd9e9dcef3fd8b9652022f49e79920e0912
Sergio <sergiolopezelio@hotmail.com>
2025-02-23 00:08:58 +0100

    Actualizada rama Sergio con archivos de master

--- prettier.config.js
+++ prettier.config.js
@@ -4,10 +4,7 @@ const config = {
 	semi: false,
 	singleQuote: true,
 	plugins: ['prettier-plugin-tailwindcss'],
-<<<<<<< HEAD
-=======
 	htmlWhitespaceSensitivity: 'ignore',
->>>>>>> 30a687d9d60e07055fad8bba69ac80a3934c9e67
 }
 
 module.exports = config
